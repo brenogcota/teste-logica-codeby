98b942b7f9ecad5394e6d0997a267ff73313904e
brenogcota <brenocota4@gmail.com>
2021-01-09 03:03:46 -0300

    feat: final version

---
+++ productStructure.js
@@ -0,0 +1,31 @@
+const products = [
+  "preto-PP", 
+  "preto-M", 
+  "preto-G", 
+  "preto-GG", 
+  "preto-GG", 
+  "branco-PP", 
+  "branco-G", 
+  "vermelho-M", 
+  "azul-XG", 
+  "azul-XG", 
+  "azul-XG", 
+  "azul-P"
+]
+
+module.exports = () => {
+  const product = products.reduce((acc, cur) => {
+    cur = cur.split('-')
+    
+    if (acc[cur[0]] != undefined && acc[cur[0]].hasOwnProperty(cur[1])) {
+      return acc = { ...acc, [cur[0]]: {...acc[cur[0]], [cur[1]]: acc[cur[0]][cur[1]]+1 } }
+    }
+
+    return acc = { ...acc, [cur[0]]: {...acc[cur[0]], [cur[1]]: 1} }
+    
+
+  }, {})
+
+  return product;
+
+}
